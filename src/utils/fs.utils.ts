48ea8ec20ea44ecc071e954150d24962d4df8550
wpadilla <wpadillappp@gmail.com>
2023-11-07 14:37:48 -0400

    God's Will caching resources

--- src/utils/fs.utils.ts
+++ src/utils/fs.utils.ts
@@ -1,12 +1,16 @@
 import fs from 'fs';
 import { BaseEntity } from '@shared/entities/BaseEntity';
 import * as path from 'path';
+import * as os from 'os';
+
+const tmpDir = os.tmpdir();
 
 export type CachedResourceType = 'companies' | 'products' | 'categories';
 export const getCachePath = (
   id: string,
   type: CachedResourceType,
-) => path.resolve(`./tmp/data/${type}/${id}.json`);
+) => path.join(tmpDir, `data/${type}/${id}.json`);
+
 export async function setCachedResource<T>(
   product: T & BaseEntity,
   type: CachedResourceType,
