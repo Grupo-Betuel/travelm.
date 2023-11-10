7a2adf93361e4fbeaa7aaff52becd1937a38dc04
wpadilla <wpadillappp@gmail.com>
2023-11-10 17:29:59 -0400

    God's Will fix performance

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 export default function NotFound() {
-  const router = useRouter();
+  // const router = useRouter();
 
   useEffect(() => {
-    router.replace('/');
+    // router.replace('/');
   });
+
   return (
     <div>
       <h1>Not Found</h1>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    notFound: true, // triggers 404
+  };
 }
