2d1b78b5541168f2aaf47eff7e526aeb2cc27cf3
wpadilla <wpadillappp@gmail.com>
2024-06-07 19:04:18 -0400

    app

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -59,7 +59,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
         Cookies.remove(AUTH_CONSTANT.TOKEN_KEY);
         setIUser(null);
         setToken(undefined);
-        location.href = '/';
+        location.href = '/auth/sign-in';
         // navigate('/auth/sign-in', {replace: true });
     };
 
