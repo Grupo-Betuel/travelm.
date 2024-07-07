1b9c9792b860ed80bd263665c412d13b6b041e04
wpadilla <wpadillappp@gmail.com>
2024-07-06 21:34:08 -0400

    improve performance

--- src/pages/[company]/index.tsx
+++ src/pages/[company]/index.tsx
@@ -93,26 +93,34 @@ import axios from 'axios';
 import { handleCachedCompany } from '../../utils/server-side.utils';
 import { saveCompanySitemap } from '../../utils/fs.utils';
 
-export default function CompanyProducts({ metadata, cachedResources, productsPerCategory }: any) {
-  const {
-    sitemapURL,
-    jsonld,
-    canonical,
-  } = handleCachedResourceHook(cachedResources);
+export default function CompanyProducts({
+  metadata,
+  cachedResources,
+  productsPerCategory,
+}: any) {
+  const { sitemapURL, jsonld, canonical } = handleCachedResourceHook(cachedResources);
   return (
     <>
-      <MetaHeaders metadata={{
-        ...metadata, jsonld, sitemapURL, canonical,
-      }}
+      <MetaHeaders
+        metadata={{
+          ...metadata,
+          jsonld,
+          sitemapURL,
+          canonical,
+        }}
       />
-      <Company company={cachedResources?.data} productsPerCategoryData={productsPerCategory} />
+      <Company
+        company={cachedResources?.data}
+        productsPerCategoryData={productsPerCategory}
+      />
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths<{ company: string }> = async () => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}api/companies`;
-  const companies: CompanyEntity[] = (await axios.get<CompanyEntity[]>(url)).data;
+  const companies: CompanyEntity[] = (await axios.get<CompanyEntity[]>(url))
+    .data;
   const companyPaths = companies.map((company) => {
     saveCompanySitemap(company);
     return {
@@ -132,15 +140,12 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const companyName = context.params?.company as string;
   const cachedResources = await handleCachedCompany(companyName);
 
-  const {
-    data: currentCompany,
-    productsPerCategory,
-  } = cachedResources;
+  const { data: currentCompany, productsPerCategory } = cachedResources;
 
   const keywords = `${currentCompany?.tags?.join(', ') || ''}`;
   return {
     props: {
-      productsPerCategory,
+      productsPerCategory: productsPerCategory || null,
       cachedResources,
       metadata: {
         keywords,
@@ -152,7 +157,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
         video: {
           url: currentCompany?.video || '',
           secureUrl: currentCompany?.video || '',
-          type: currentCompany?.video?.includes('mp4') ? 'video/mp4' : 'video/ogg',
+          type: currentCompany?.video?.includes('mp4')
+            ? 'video/mp4'
+            : 'video/ogg',
         },
       },
     },
