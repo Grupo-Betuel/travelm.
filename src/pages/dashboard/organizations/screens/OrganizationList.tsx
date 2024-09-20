528245250a83aade6a7eb0ab7cec969269df80aa
Antony08Dev <antonydeveloper08@gmail.com>
2024-09-20 13:00:48 -0400

    confirmar antes de eliminar

--- src/pages/dashboard/organizations/screens/OrganizationList.tsx
+++ src/pages/dashboard/organizations/screens/OrganizationList.tsx
@@ -1,32 +1,53 @@
-import {Avatar, Button, Card, CardBody, CardHeader, Typography} from "@material-tailwind/react";
+import { Avatar, Button, Card, CardBody, CardHeader, Typography } from "@material-tailwind/react";
 import React from "react";
-import {useOrganizationHandler} from "../../../../hooks/useOrganizationHandler";
-import {IOrganization} from "../../../../models/organizationModel";
-import {IoReload} from "react-icons/io5";
+import { useOrganizationHandler } from "../../../../hooks/useOrganizationHandler";
+import { IOrganization } from "../../../../models/organizationModel";
+import { IoReload } from "react-icons/io5";
 import ProtectedElement from "../../../../components/ProtectedElement";
-import {UserRoleTypes} from "../../../../models/interfaces/userModel";
+import { UserRoleTypes } from "../../../../models/interfaces/userModel";
+import { useConfirmAction } from "@/hooks/useConfirmActionHook";
+import { CommonConfirmActions, CommonConfirmActionsDataTypes } from "@/models/common";
 
 export const OrganizationList: React.FC = () => {
     const {
         organizationForm,
         toggleHandleOrganization,
-        setSelectedOrganization,
         organizations,
         refetchOrganizations,
         onEditOrganization,
         onDeleteOrganization
     } = useOrganizationHandler({});
-    console.log('organizations', organizations);
+
+    // Acción confirmada (en este caso, eliminación)
+    const onConfirmAction = (type?: CommonConfirmActions, data?: CommonConfirmActionsDataTypes<IOrganization>) => {
+        switch (type) {
+            case 'delete':
+                onDeleteOrganization(data as IOrganization); // Confirmar la eliminación
+                break;
+        }
+    };
+
+    // Acción denegada (en este caso, puedes dejarla vacía si no haces nada al cancelar)
+    const onDeniedAction = (type?: CommonConfirmActions, data?: CommonConfirmActionsDataTypes<IOrganization>) => {
+        // Lógica opcional si se cancela la acción
+    };
+
+    // Hook de confirmación
+    const {
+        handleSetActionToConfirm,
+        ConfirmDialog
+    } = useConfirmAction<CommonConfirmActions, CommonConfirmActionsDataTypes<IOrganization>>(onConfirmAction, onDeniedAction);
+
     return (
         <div>
             <Card>
                 <CardHeader variant="gradient" color="gray" className="mb-8 p-6 flex justify-between">
-                    <div className="flex items-center ">
+                    <div className="flex items-center">
                         <Typography variant="h4" color="white">
                             Organizaciones
                         </Typography>
                         <Button variant="text" color="white" onClick={refetchOrganizations}>
-                            <IoReload className="w-[18px] h-[18px] cursor-pointer"/>
+                            <IoReload className="w-[18px] h-[18px] cursor-pointer" />
                         </Button>
                     </div>
                     <ProtectedElement roles={[UserRoleTypes.ADMIN]}>
@@ -38,14 +59,8 @@ export const OrganizationList: React.FC = () => {
                         <thead>
                         <tr>
                             {["Logo", "Nombre", "Email", "Tipo", "Acciones"].map((el) => (
-                                <th
-                                    key={el}
-                                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        className="text-[11px] font-bold uppercase text-blue-gray-400"
-                                    >
+                                <th key={el} className="border-b border-blue-gray-50 py-3 px-5 text-left">
+                                    <Typography variant="small" className="text-[11px] font-bold uppercase text-blue-gray-400">
                                         {el}
                                     </Typography>
                                 </th>
@@ -53,58 +68,59 @@ export const OrganizationList: React.FC = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        {organizations?.map(
-                            (organization: IOrganization, key: number) => {
-                                const className = `py-3 px-5 ${
-                                    key === organizations.length - 1
-                                        ? ""
-                                        : "border-b border-blue-gray-50"
-                                }`;
-                                const {logo, name, email, type} = organization;
+                        {organizations?.map((organization: IOrganization, key: number) => {
+                            const className = `py-3 px-5 ${
+                                key === organizations.length - 1 ? "" : "border-b border-blue-gray-50"
+                            }`;
+                            const { logo, name, contact, type } = organization;
 
-                                return (
-                                    <tr key={`${name}-${key}`}>
-                                        <td className={className}>
-                                            <div className="flex items-center gap-4">
-                                                <Avatar src={logo?.content} alt={name} size="sm" variant="rounded"/>
-                                            </div>
-                                        </td>
-                                        <td className={className}>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-semibold"
+                            return (
+                                <tr key={`${name}-${key}`}>
+                                    <td className={className}>
+                                        <div className="flex items-center gap-4">
+                                            <Avatar src={logo?.content} alt={name} size="sm" variant="rounded" />
+                                        </div>
+                                    </td>
+                                    <td className={className}>
+                                        <Typography variant="small" color="blue-gray" className="font-semibold">
+                                            {name}
+                                        </Typography>
+                                    </td>
+                                    <td className={className}>
+                                        <Typography className="text-xs font-normal text-blue-gray-500">
+                                            {contact?.email}
+                                        </Typography>
+                                    </td>
+                                    <td className={className}>
+                                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                                            {type}
+                                        </Typography>
+                                    </td>
+                                    <td className={className}>
+                                        <div className="flex items-center gap-2">
+                                            <Button color="blue" size="sm" onClick={() => onEditOrganization(organization)}>
+                                                Editar
+                                            </Button>
+                                            <Button
+                                                color="red"
+                                                size="sm"
+                                                onClick={() => handleSetActionToConfirm('delete')(
+                                                    organization
+                                                )}
                                             >
-                                                {name}
-                                            </Typography>
-                                        </td>
-                                        <td className={className}>
-                                            <Typography className="text-xs font-normal text-blue-gray-500">
-                                                {email}
-                                            </Typography>
-                                        </td>
-                                        <td className={className}>
-                                            <Typography className="text-xs font-semibold text-blue-gray-600">
-                                                {type}
-                                            </Typography>
-                                        </td>
-                                        <td className={className}>
-                                            <div className="flex items-center gap-2">
-                                                <Button color="blue" size="sm"
-                                                        onClick={() => onEditOrganization(organization)}>Editar</Button>
-                                                <Button color="red" size="sm"
-                                                        onClick={() => onDeleteOrganization(organization)}>Eliminar</Button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                );
-                            }
-                        )}
+                                                Eliminar
+                                            </Button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                         </tbody>
                     </table>
                 </CardBody>
             </Card>
             {organizationForm}
+            <ConfirmDialog />
         </div>
     );
-}
+};
